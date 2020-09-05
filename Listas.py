180c825171db77418f51703902504120d304c49e
Vicente Canales <vicente.canalesr@gmail.cl>
2020-09-05 15:14:33 -0400

    Tarea4 Final

---
+++ Listas.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Notas Finales
+
+# In[9]:
+
+
+diccionario=dict(Nombre1='Fresia',Nombre2='Vicente',Nombre3='Dante',Nombre4='Leon',
+                 Apellido1='Contreras',Apellido2='Canales',Apellido3='Morales',Apellido4='Venegas',
+                Nota1a='6.5',Nota1b='5.0',Nota2a='6.7',Nota2b='5.5',Nota3a='7.0',Nota3b='7.0',Nota4a='3.5',Nota4b='4.5',
+                Curso1='Física',Curso2='Programación')
+
+
+# ### Alumno:
+
+# In[28]:
+
+
+print(diccionario['Nombre1'],diccionario['Apellido1'],diccionario['Curso1'],diccionario['Nota1a'],diccionario['Curso2'],diccionario['Nota1b'])
+
+
+# In[29]:
+
+
+print(diccionario['Nombre2'],diccionario['Apellido2'],diccionario['Curso1'],diccionario['Nota2a'],diccionario['Curso2'],diccionario['Nota2b'])
+
+
+# In[30]:
+
+
+print(diccionario['Nombre3'],diccionario['Apellido3'],diccionario['Curso1'],diccionario['Nota3a'],diccionario['Curso2'],diccionario['Nota3b'])
+
+
+# In[31]:
+
+
+print(diccionario['Nombre4'],diccionario['Apellido4'],diccionario['Curso1'],diccionario['Nota4a'],diccionario['Curso2'],diccionario['Nota4b'])
+
---
+++ OperacionesMatematicas.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+#variables
+x=1
+y=2
+z=3
+
+
+# # Operaciones Matemáticas   
+# 
+
+# ## 1) Suma: 
+# ### sea x=1 e y=2, entonces x+y =
+
+# In[31]:
+
+
+print(f"{x+y} (es el resultado de sumar {x} con {y})")
+
+
+# ## 2) Resta: 
+# ### sea z=3 e y=2, entonces z-y=
+
+# In[32]:
+
+
+print(f"{z-y} (es el resultado de restar {z} con {y})")
+
+
+# ## 3)multiplicación:
+# ### sea y=2 y z=3, entonces y*z=
+
+# In[33]:
+
+
+print(f"{z*y} (es el resultado de multiplicar {y} por {z})")
+
+
+# ## 4) Division:
+# ### sea z=3 e y=2, entonces z/y =
+
+# In[34]:
+
+
+print(f"{z/y} (es el resultado de dividir {z} entre {y})")
+
+
+# ## 5) Operación Modulo:
+# ### sea z=3 e y=2, entonces z%y=
+
+# In[36]:
+
+
+print(f"{z%y} (es el resultado de {z}%{y}, ya que el resto de aquella operacion es 1)")
+
